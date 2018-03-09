139b508161aa1b093da9f7373dc83ee45943b50a
CfyKty <37198821+CfyKty@users.noreply.github.com>
2018-03-10 10:46:00 +1100

    Add files via upload

---
+++ Bot.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Bot
+{
+    public static void main(String[] args)
+    {
+        /* The boolean passed to the Configuration constructor dictates whether or not the
+           bot is connecting to the prod or test exchange. Be careful with this switch! */
+        Configuration config = new Configuration(true);
+        try
+        {
+            Socket skt = new Socket(config.exchange_name(), config.port());
+            BufferedReader from_exchange = new BufferedReader(new InputStreamReader(skt.getInputStream()));
+            PrintWriter to_exchange = new PrintWriter(skt.getOutputStream(), true);
+
+            /*
+              A common mistake people make is to to_exchange.println() > 1
+              time for every from_exchange.readLine() response.
+              Since many write messages generate marketdata, this will cause an
+              exponential explosion in pending messages. Please, don't do that!
+            */
+            to_exchange.println(("HELLO " + config.team_name).toUpperCase());
+            String reply = from_exchange.readLine().trim();
+            System.err.printf("The exchange replied: %s\n", reply);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace(System.out);
+        }
+    }
+}
